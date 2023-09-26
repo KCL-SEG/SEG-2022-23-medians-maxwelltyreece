68680bb806b42b8a09676b9d0bfee4386817e459
Maxwell Martin <maxwelltymartin@gmail.com>
2023-09-26 13:30:35 +0100

    Attempt 3

--- median.py
+++ median.py
@@ -17,4 +17,4 @@ elif len(numbers) % 2 == 0:
     print( (numbers[int( (len(numbers)/2)-1 )] + numbers[int(len(numbers)/2)]) / 2)
 
 else:
-    print(numbers[int( (len(numbers)+1) / 2 )])
+    print(numbers[int( (len(numbers)-1) / 2 )])
